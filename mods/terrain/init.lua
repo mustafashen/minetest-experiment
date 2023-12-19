83fabf5f33285e048b04e51ab5f0c08714b5250c
mustafa <mustafasen@tuta.io>
2023-12-19 20:36:26 +0300

    new tools and nodes

--- mods/terrain/init.lua
+++ mods/terrain/init.lua
@@ -2,24 +2,190 @@
 minetest.register_node('terrain:stone', {
   description = "mapgen_stone",
 	tiles = {"stone.png"},
-  groups = { oddly_breakable_by_hand = 3 },
+  groups = { oddly_breakable_by_hand = 1, cracky = 1 },
   is_ground_content = true
 })
 
-minetest.register_node('terrain:water_source', {
-  description = 'mapgen_water_source',
-  tiles = { 'water_source.png' },
-  groups = { oddly_breakable_by_hand = 0 },
-  is_ground_content = true
+minetest.register_node("terrain:water_source", {
+	drawtype = "liquid",
+	waving = 3,
+	tiles = {
+		{
+			name = "water.png",
+			backface_culling = false,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 5.0,
+			},
+		},
+		{
+			name = "water.png",
+			backface_culling = true,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 5.0,
+			},
+		},
+	},
+	use_texture_alpha = "blend",
+	paramtype = "light",
+	walkable = false,
+	pointable = false,
+	diggable = false,
+	buildable_to = true,
+	is_ground_content = false,
+	drop = "",
+	drowning = 0,
+	liquidtype = "source",
+	liquid_alternative_flowing = "terrain:water_flowing",
+	liquid_alternative_source = "terrain:water_source",
+	liquid_viscosity = 1,
+	post_effect_color = {a = 103, r = 30, g = 60, b = 90},
+	groups = {},
 })
 
-minetest.register_node('terrain:river_water_source', {
-  description = 'mapgen_river_water_source”',
-  tiles = { 'river_water_source.png' },
-  groups = { oddly_breakable_by_hand = 0 },
-  is_ground_content = true
+minetest.register_node("terrain:water_flowing", {
+	drawtype = "flowingliquid",
+	waving = 3,
+	special_tiles = {
+		{
+			name = "water_flow.png",
+			backface_culling = false,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 1,
+			},
+		},
+		{
+			name = "water_flow.png",
+			backface_culling = true,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 1,
+			},
+		},
+	},
+	use_texture_alpha = "blend",
+	paramtype = "light",
+	paramtype2 = "flowingliquid",
+	walkable = false,
+	pointable = false,
+	diggable = false,
+	buildable_to = true,
+	is_ground_content = false,
+	drop = "",
+	drowning = 0,
+	liquidtype = "flowing",
+	liquid_alternative_flowing = "terrain:water_flowing",
+	liquid_alternative_source = "terrain:water_source",
+	liquid_viscosity = 1,
+	post_effect_color = {a = 103, r = 30, g = 60, b = 90},
+	groups = {},
 })
 
+
+minetest.register_node("terrain:river_water_source", {
+	drawtype = "liquid",
+	tiles = {
+		{
+			name = "water.png",
+			backface_culling = false,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 2.0,
+			},
+		},
+		{
+			name = "water.png",
+			backface_culling = true,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 2.0,
+			},
+		},
+	},
+	use_texture_alpha = "blend",
+	paramtype = "light",
+	walkable = false,
+	pointable = false,
+	diggable = false,
+	buildable_to = true,
+	is_ground_content = false,
+	drop = "",
+	drowning = 0,
+	liquidtype = "source",
+	liquid_alternative_flowing = "terrain:river_water_flowing",
+	liquid_alternative_source = "terrain:river_water_source",
+	liquid_viscosity = 1,
+	-- Not renewable to avoid horizontal spread of water sources in sloping
+	-- rivers that can cause water to overflow riverbanks and cause floods.
+	-- River water source is instead made renewable by the 'force renew'
+	-- option used in the 'bucket' mod by the river water bucket.
+	liquid_renewable = false,
+	liquid_range = 2,
+	post_effect_color = {a = 103, r = 30, g = 76, b = 90},
+	groups = {},
+})
+
+minetest.register_node("terrain:river_water_flowing", {
+	drawtype = "flowingliquid",
+	special_tiles = {
+		{
+			name = "water_flow.png",
+			backface_culling = false,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 1,
+			},
+		},
+		{
+			name = "water_flow.png",
+			backface_culling = true,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 1,
+			},
+		},
+	},
+	use_texture_alpha = "blend",
+	paramtype = "light",
+	paramtype2 = "flowingliquid",
+	walkable = false,
+	pointable = false,
+	diggable = false,
+	buildable_to = true,
+	is_ground_content = false,
+	drop = "",
+	drowning = 0,
+	liquidtype = "flowing",
+	liquid_alternative_flowing = "terrain:river_water_flowing",
+	liquid_alternative_source = "terrain:river_water_source",
+	liquid_viscosity = 1,
+	liquid_renewable = false,
+	liquid_range = 2,
+	post_effect_color = {a = 103, r = 30, g = 76, b = 90},
+	groups = {},
+})
+
+
+-- Mapgen aliases
 minetest.register_alias('mapgen_stone', 'terrain:stone')
 minetest.register_alias('mapgen_water_source', 'terrain:water_source')
 minetest.register_alias('mapgen_river_water_source', 'terrain:river_water_source')
+
--- mods/tools/init.lua
+++ mods/tools/init.lua
@@ -5,8 +5,28 @@ minetest.override_item("", {
 		full_punch_interval = 10,
 		max_drop_level = 0,
 		groupcaps = {
-			oddly_breakable_by_hand = {times={[1]=3.50,[2]=2.00,[3]=0}, uses=0}
+			oddly_breakable_by_hand = {times={[1]=3.50}, uses=0}
 		},
 		damage_groups = {unarmed=1},
+	}
+})
+
+minetest.register_tool("tools:pick_wood", {
+	description = "Wooden Pickaxe",
+	inventory_image = "default_tool_woodpick.png",
+	tool_capabilities = {
+		full_punch_interval = 1.2,
+		max_drop_level=0,
+		groupcaps={
+			cracky = {times={[1]=0.1}, uses=10, maxlevel=1},
+		},
+		damage_groups = {unarmed=2},
+	},
+})
+
+minetest.register_craft({
+	output = "tools:pick_wood",
+	recipe = {
+		{"terrain:stone"}
 	}
 })
